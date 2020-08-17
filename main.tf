34386749e3ef14c3f35a61cc51f2d06e188860aa
Jamie Causey <jamiecausey@jamies-mbp.lan>
2020-08-17 15:18:59 -0500

    added main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "vpcname" {
+    type = string
+    description = "Enter VPC Name: "
+}
+
+resource "aws_vpc" "jcvpc" {
+    cidr_block = "192.168.0.0/24"
+    tags = {
+        name = var.vpcname
+
+    }
+}
+
+output "jcout" {
+    value = aws_vpc.jcvpc
+}
