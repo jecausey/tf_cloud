d75c9ce58253ed1f9e73f9815f25df62e594cfff
Jamie Causey <jamiecausey@jamies-mbp.lan>
2020-08-18 13:12:22 -0500

    removed var

--- main.tf
+++ main.tf
@@ -2,16 +2,15 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable "vpcname" {
-    type = string
-    description = "Enter VPC Name: "
-}
+#variable "vpcname" {
+#    type = string
+#    description = "Enter VPC Name: "
+#}
 
 resource "aws_vpc" "jcvpc" {
     cidr_block = "192.168.0.0/24"
     tags = {
-        name = var.vpcname
-
+        name = jhcvpc01
     }
 }
 
